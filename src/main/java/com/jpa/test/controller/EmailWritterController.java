ef541dff7aa75ad621708a7e53fde76c5a45f553
Anweshapal_ <86425625+Anweshapal@users.noreply.github.com>
2025-11-06 14:07:50 +0530

    Update EmailWritterController.java

--- src/main/java/com/jpa/test/controller/EmailWritterController.java
+++ src/main/java/com/jpa/test/controller/EmailWritterController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import com.jpa.test.EmailRequest;
 import com.jpa.test.service.EmailWritterService;
 
@@ -25,5 +25,9 @@ public class EmailWritterController {
 		String response =emailWritterService.generateEmailReply(emailRequest);
 		return ResponseEntity.ok(response);
 	}
+	@GetMapping({"", "/"})
+	    public String testBrowser() {
+	        return "✅ Smart Email Assistant API is Running!";
+	    }
 	
 }
